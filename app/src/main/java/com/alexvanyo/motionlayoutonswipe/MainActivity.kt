505ce7461c4170e35a13f86f859fe0077fc8c9ff
Alex Vanyo <vanyoalex@gmail.com>
2021-01-04 19:53:19 -0600

    Reproduce bug

---
+++ app/src/main/java/com/alexvanyo/motionlayoutonswipe/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.alexvanyo.motionlayoutonswipe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.text)
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(motionLayout: MotionLayout, startId: Int, endId: Int) {
+                Log.d("MainActivity", "onTransitionStarted: startId=$startId, endId=$endId")
+            }
+
+            override fun onTransitionChange(motionLayout: MotionLayout, startId: Int, endId: Int, progress: Float) {
+                Log.d("MainActivity", "onTransitionStarted: startId=$startId, endId=$endId, progress=$progress")
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+                Log.d("MainActivity", "onTransitionStarted: currentId=$currentId")
+
+                text.text = (when (currentId) {
+                    R.id.collapsed -> "Collapsed!"
+                    R.id.expanded -> "Expanded!"
+                    else -> throw IllegalStateException("Unknown constraint set id!")
+                })
+            }
+
+            override fun onTransitionTrigger(motionLayout: MotionLayout, triggerId: Int, positive: Boolean, progress: Float) {
+                Log.d("MainActivity", "onTransitionStarted: triggerId=$triggerId, positive=$positive, progress=$progress")
+            }
+        })
+    }
+}
